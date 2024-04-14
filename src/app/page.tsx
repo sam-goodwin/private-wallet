3f29616eaa96d924113ca07b593503149580b736
Sam Goodwin <sam@packyak.ai>
2024-04-13 20:14:41 -0700

    stash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-// import { Resource } from "sst";
 import { PrivateKey } from "o1js";
 
 export default async function Root() {
-  const isLive = !!process.env.SST_LIVE;
-
-  // const privateKey = PrivateKey.fromBase58(Resource.RootKey.value);
   const privateKey = PrivateKey.random();
+
   return (
     <main>
-      <p>{isLive ? "Live" : "Not Live"}</p>
       <h1>Public Key</h1>
-      <span>{privateKey.toPublicKey().toBase58()}</span>
+      <span>{privateKey?.toPublicKey().toBase58()}</span>
     </main>
   );
 }
